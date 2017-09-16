32f77722f254d3181c63a9537fe35b19c40b36a9
angelokofficial <angelok2015.mc@gmail.com>
2017-09-17 00:51:50 +0300

    autopin scrpit

---
+++ src/autopin.js
@@ -0,0 +1,7 @@
+	var TelegramBot = require('node-telegram-bot-api');
+	var token = ''; //токен вашего бота
+	var bot = new TelegramBot(token, {polling: false});
+
+	var chatid = msg.chat.id;
+	bot.pinChatMessage(chatid, msg.message_id + 5).catch(function(error) {
+		bot.sendMessage(chatid, 'Не могу запиннить, я без админки :c'); })
