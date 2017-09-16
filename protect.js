f47e687ceffd5638add3c6c07f141b95ab4beb83
angelokofficial <angelok2015.mc@gmail.com>
2017-09-17 00:50:53 +0300

    delete

--- protect.js
+++
@@ -1,9 +0,0 @@
-var replace = require("replace");
-replace({
-  regex: "var isSpin = false;",
-  replacement: "var isSpin = true;",
-  paths: ['core.js'],
-  recursive: false,
-  silent: false,
-});
-console.log('activated');
--- resetgame.js
+++
@@ -1,8 +0,0 @@
-var replace = require("replace");
-replace({
-  regex: "var isSpin = true;",
-  replacement: "var isSpin = false;",
-  paths: ['core.js'],
-  recursive: false,
-  silent: false,
-});
