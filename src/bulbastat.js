3c32fdc001d42f62aaf7f1a908cdc3e0001549c2
angelokofficial <angelok2015.mc@gmail.com>
2017-09-21 19:18:35 +0300

    BOTD stats

--- src/bulbastat.js
+++ src/bulbastat.js
@@ -1,13 +1,13 @@
-var TelegramBot = require('node-telegram-bot-api');
-var token = ''; //токен вашего бота
-var bot = new TelegramBot(token, {polling: false});
-var chatid = msg.chat.id;
-//var ipc = -1001133868291; //ed
-var ipc = -1001102571478; //ipc
+	var TelegramBot = require('node-telegram-bot-api');
+	var token = ''; //токен вашего бота
+	var bot = new TelegramBot(token, {polling: false});
+	var chatid = msg.chat.id;
+//	var ipc = -1001133868291; //ed
+	var ipc = -1001102571478; //ipc
 
-if (chatid != ipc) {
-	bot.sendSticker(chatid, 'CAADAgADwQMAAtQlfAnWSOPgAs6UbwI', {reply_to_message_id : msg.message_id});
-} else {
-	bot.sendMessage(chatid, 'Топ бульбочек ин зе ворлд:\n\n1. <code>@EgoruOff</code> - <b>4 раза</b>\n2. <code>@chorogon</code> - <b>4 раза</b>\n3. <code>@fuckussr</code> - <b>4 раза</b>\n4. <code>@Infernituum</code> - <b>3 раза</b>\n5. <code>@OctoNezd</code> - <b>3 раза</b>\n6. <code>@KosBeg</code> - <b>3 раза</b>\n7. <code>@unknwn404</code> - <b>3 раза</b>\n8. <code>@angelokofficial</code> - <b>3 раза</b>\n9. <code>@Leckk</code> - <b>3 раза</b>\n10. <code>@Lyubimych</code> - <b>3 раза</b>\n11. <code>@ZaMIk</code> - <b>2 раза</b>\n12. <code>@reloadingfoxofficial</code> - <b>2 раза</b>\n13. <code>@spaaaaacefan</code> - <b>2 раза</b>\n14. <code>@MyDearEvr</code> - <b>1 раз</b>\n15. <code>@SuperPuperSteve</code> - <b>1 раз</b>\n16. <code>@gershik</code> - <b>1 раз</b>\n17. <code>@zhalka</code> - <b>1 раз</b>' ,{parse_mode: "HTML"});
-	console.log ('[I] ' + 'Reply to the message from ' + '@' + msg.from.username + ' ' + '(' + msg.from.id + ')');
+	if (chatid != ipc) {
+		bot.sendSticker(chatid, 'CAADAgADwQMAAtQlfAnWSOPgAs6UbwI', {reply_to_message_id : msg.message_id});
+	} else {
+		bot.sendMessage(chatid, 'Топ бульбочек ин зе ворлд:\n\n1. <code>@EgoruOff</code> - <b>4 раза</b>\n2. <code>@chorogon</code> - <b>4 раза</b>\n3. <code>@fuckussr</code> - <b>4 раза</b>\n4. <code>@unknwn404</code> - <b>4 раза</b>\n5. <code>@Infernituum</code> - <b>3 раза</b>\n6. <code>@OctoNezd</code> - <b>3 раза</b>\n7. <code>@KosBeg</code> - <b>3 раза</b>\n8. <code>@angelokofficial</code> - <b>3 раза</b>\n9. <code>@Leckk</code> - <b>3 раза</b>\n10. <code>@Lyubimych</code> - <b>3 раза</b>\n11. <code>@ZaMIk</code> - <b>2 раза</b>\n12. <code>@reloadingfoxofficial</code> - <b>2 раза</b>\n13. <code>@spaaaaacefan</code> - <b>2 раза</b>\n14. <code>@MyDearEvr</code> - <b>1 раз</b>\n15. <code>@SuperPuperSteve</code> - <b>1 раз</b>\n16. <code>@gershik</code> - <b>1 раз</b>\n17. <code>@zhalka</code> - <b>1 раз</b>\n18. <code>@amxlosted217</code> - <b>1 раз</b>' ,{parse_mode: "HTML"});
+		console.log ('[I] ' + 'Reply to the message from ' + '@' + msg.from.username + ' ' + '(' + msg.from.id + ')');
 };
