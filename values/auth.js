888380c9ec7d9602a258790746e1e3678b428af1
angelokofficial <angelok2015.mc@gmail.com>
2017-09-24 15:07:10 +0300

    authorization keys like token

---
+++ values/auth.js
@@ -0,0 +1,6 @@
+// Insert your token from @BotFather
+module.exports.token = '';
+// Insert api key from your project in Google Cloud Platform (for google search command)
+module.exports.key = '';
+// Insert cx from your custom search engine in Google Cloud Platform (for google search command)
+module.exports.cx = '';
