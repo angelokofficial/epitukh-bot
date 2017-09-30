de0772fb888d6182526ac1d1c02c375d3d34e760
Vitya Angelov <angelok2015.mc@gmail.com>
2017-10-01 01:30:33 +0300

    BOTD
    fixes

--- bulba/exportingvariables.js
+++ bulba/exportingvariables.js
@@ -1,5 +1,5 @@
 var date = new Date();
 var d = date.getDate();
 if (d < 10) d = '0' + d
-module.exports.day = 24;
-module.exports.bulba = '<b>@OctoNezd</b>';
+module.exports.day = 01;
+module.exports.bulba = '';
