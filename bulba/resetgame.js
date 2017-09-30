f8258dbf9cf28098487f8a1e9da1f542badae255
Vitya Angelov <angelok2015.mc@gmail.com>
2017-10-01 01:30:59 +0300

    del

--- bulba/resetgame.js
+++
@@ -1,8 +0,0 @@
-var replace = require("replace");
-replace({
-  regex: "var isSpin = true;",
-  replacement: "var isSpin = false;",
-  paths: ['bulba/bulba.js'],
-  recursive: false,
-  silent: false,
-});
